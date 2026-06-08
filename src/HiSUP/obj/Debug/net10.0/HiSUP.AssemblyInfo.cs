99d0dde15f7477ddc82a7c2a5985f37ceb1320a4
24-cs-088-cmyk <24-cs-088@student.hitecuni.edu.pk>
2026-06-08 13:16:50 +0500

    [Web] Fix table name mappings, all module pages working

--- src/HiSUP/obj/Debug/net10.0/HiSUP.AssemblyInfo.cs
+++ src/HiSUP/obj/Debug/net10.0/HiSUP.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HiSUP")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+328e3b796c31c5d53f2fcc9ce3f86562286b6a2f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7c068848bb0ff32ba4789d98951abf5f110a9be")]
 [assembly: System.Reflection.AssemblyProductAttribute("HiSUP")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HiSUP")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
